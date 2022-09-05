bb04fd539d0a505c2616a27944c2cbceea2ce9a9
JS15 <apocalypticpillow@gmail.com>
2022-09-05 14:50:49 +0800

    added reading functionality for grouped_choices. cleaned code a bit heh

--- exams.jsx
+++ exams.jsx
@@ -20,10 +20,15 @@ let o = O[0];
 console.log("[" + L + "]\n\n");
 console.log("-\n\n[" + o.name + "]");
 
-let wr = "[❌ incorrectly answered questions]: \n";
-let un = "[unanswered questions]: \n";
+let wr = "[❌ incorrectly answered questions]:\n";
+let un = "[unanswered questions]:\n";
+let _wr = wr
+let _un = un
+
 for (let i = 0; i < Object.keys(o.last_attempt_scores).length; i++) {
     let _a = o.last_attempt_scores[i];
+    let nextIsCorrect = o.last_attempt_scores[i + 1] == "previously_correct";
+    let nextIsAnswered = o.last_attempt_scores[i + 1] != null;
 
     console.log(i + 1,
         (_a == "previously_correct" ? "✔️ correct" :
@@ -33,10 +38,10 @@ for (let i = 0; i < Object.keys(o.last_attempt_scores).length; i++) {
     );
 
     if (_a == "incorrect") {
-        wr = wr + (i + 1).toString() + "\n";
+        wr = (wr + (i + 1).toString()) + (!nextIsCorrect ? "\n" : "\n\n");
     }
     else if (_a == null) {
-        un = un + (i + 1).toString() + "\n";
+        un = (un + (i + 1).toString()) + (!nextIsAnswered ? "\n" : "\n\n");
     }
 }
 
@@ -47,5 +52,5 @@ console.log(
     "\n| correctly answered questions", o.questions_answered_count * (o.correct_percent / 100), "/", o.questions_count
 );
 
-if (wr != "[❌ incorrectly answered questions]: \n") console.log("\n" + wr);
-if (un != "[unanswered questions]: \n") console.log("\n" + un);
+if (wr != _wr) console.log("\n" + wr);
+if (un != _un) console.log("\n" + un);
